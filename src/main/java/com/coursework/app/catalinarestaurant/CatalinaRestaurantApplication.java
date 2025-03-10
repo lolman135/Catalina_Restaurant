9ab84ca0134fa43d8271d0b9a76f0d747e17e6ca
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2025-03-11 00:18:26 +0200

    Initial commit

---
+++ src/main/java/com/coursework/app/catalinarestaurant/CatalinaRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.app.catalinarestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalinaRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalinaRestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coursework/app/catalinarestaurant/CatalinaRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.app.catalinarestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalinaRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
